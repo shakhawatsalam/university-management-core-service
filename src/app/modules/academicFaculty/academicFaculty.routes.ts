edb94722bfefa7229dd052e4dff812fb91fb7a7c
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-08-20 11:43:38 +0600

    [Acacemic Faculty is done]

--- src/app/modules/academicFaculty/academicFaculty.routes.ts
+++ src/app/modules/academicFaculty/academicFaculty.routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
-import { AcademicFacultyController } from './academicFaculty.contoller';
+
+import { AcademicFacultyController } from './academicFaculty.controller';
 import { AcademicFacultyValidation } from './academicFaculty.validation';
 
 const router = express.Router();
