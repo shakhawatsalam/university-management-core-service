b2c47976cde1ffc0973788bee7d0367bdbe2f301
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-08-29 07:32:57 +0600

    [Some Changes In offredCourseSection Service]

--- src/app/modules/offredCourseSection/offredCourseSection.service.ts
+++ src/app/modules/offredCourseSection/offredCourseSection.service.ts
@@ -1,9 +1,18 @@
 import { OffredCourseSection } from '@prisma/client';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import prisma from '../../../shared/prisma';
 
-const insertIntoDB = async (
-  data: OffredCourseSection
-): Promise<OffredCourseSection> => {
+const insertIntoDB = async (data: any): Promise<OffredCourseSection> => {
+  const isExistOffredCourse = await prisma.offredCourse.findFirst({
+    where: {
+      id: data.offredCourseId,
+    },
+  });
+  if (!isExistOffredCourse) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Offred Course Does Not Exist');
+  }
+  data.semesterRegistrationId = isExistOffredCourse.semesterRegistrationId;
   const result = await prisma.offredCourseSection.create({
     data,
   });
